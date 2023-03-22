c86284cc9ad48d51fcab17eb424e6a43a4f054ca
Hikari Kawai <121547505+hikarikawai@users.noreply.github.com>
2023-03-23 01:04:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
 					logging: false,
 			  }),
 	HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-	SUDO: process.env.SUDO || '',
+	SUDO: process.env.SUDO || '6289677134384' '6288291003123',
 	HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
 	HEROKU_API_KEY: process.env.HEROKU_API_KEY,
 	BRANCH: 'master',
